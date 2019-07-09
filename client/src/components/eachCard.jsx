f10f413eb63888f8404c8fffe64203d5a9bf2b49
Jared Bourget <jared.m.bourget@gmail.com>
2019-07-09 14:12:02 -0600

    Cards are looking better

--- client/src/components/eachCard.jsx
+++ client/src/components/eachCard.jsx
@@ -4,9 +4,16 @@ export default function EachCard (restaurant) {
 	console.log(restaurant.restaurant)
   return (
 		<div className="card">
-		<div className="container">
 		<img src={restaurant.restaurant.img_url}></img>
-			<h4><b>{restaurant.restaurant.name}</b></h4> 
+		<div className="container">
+			<h4>
+				<div><b>{restaurant.restaurant.name}</b></div>
+				<div>{restaurant.restaurant.style} {restaurant.restaurant.price}</div>
+			</h4> 
+			<div className="zagatImg">
+				<img src="https://www.zagat.com/assets/img/z-logo-icon-red.svg"></img>
+				<span>FOOD {restaurant.restaurant.rating}</span>
+			</div>
 			<p>{restaurant.restaurant.description}</p> 
 		</div>
 	</div>
