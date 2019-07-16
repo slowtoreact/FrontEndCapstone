dc83a583e25287aeaf2a4a5b1aba696d57ae9e29
Jared Bourget <jared.m.bourget@gmail.com>
2019-07-16 10:59:27 -0600

    Listening on port 3001

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./client/dist'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
-const port = 3000;
+const port = 3001;
 
 app.post('/', (req, res) => {
     db.save(req.body)
